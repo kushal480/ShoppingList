80030b6fe54980727efc74a3325a7413a832d0fd
Srikanth Krishna Kolli <ksrikanth.kolli@gmail.com>
2019-06-20 00:49:05 -0700

    logic done
    More CSS required on buttons and <li> elemtns

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var li = document.querySelector("li");
+var listItems = document.getElementsByTagName("li");
 
-function inputLength() {
-	return input.value.length;
+function checkInput() {
+    return input.value;
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+
+    var btn = document.createElement("button");
+    btn.appendChild(document.createTextNode("Delete"));
+    li.appendChild(btn);
+    btn.onclick = removeParent;
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+    if (checkInput() !== "") {
+        createListElement();
+    }    
 }
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+function addListAfterEnter() {
+        if (checkInput() !== "" && event.keyCode === 13) {
+            createListElement();
+         }
 }
 
-function toggleDone() {
-    document.querySelector("li").classList.toggle("done");
+function listLength() {
+    return listItems.length;
 }
 
-li.addEventListener("click", toggleDone);
+function deleteButton() {
+    var btn = document.createElement("button");
+    btn.appendChild(document.createTextNode("Delete!"));
+    listItems[i].appendChild(btn);
+    btn.onclick = removeParent;
+}
 
+function removeParent(evt) {
+    evt.target.parentNode.remove();
+}
+
+for (i = 0; i < listLength(); i++) {
+    deleteButton();
+}
 button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterEnter);
 
-input.addEventListener("keypress", addListAfterKeypress);
+ul.onclick = function (event) {
+    var target = event.target;
+    target.classList.toggle("done");
+}
